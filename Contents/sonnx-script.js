f5c572d2ac132959292f5cd036827a5ab19e6791
xuansonkrt <43500435+xuansonkrt@users.noreply.github.com>
2019-06-07 00:11:38 +0700

    chọn chỗ và giỏ vé

--- Contents/sonnx-script.js
+++ Contents/sonnx-script.js
@@ -8,9 +8,5 @@ $(document).ready(function(){
 
 
 
-    $(".toggle-sidebar").click(function () {    
-        $("#sidebar").toggleClass("collapsed"); 
-        $("#content").toggleClass("col-md-12 col-md-9");    
-                        return false;   
-    }); 
+    $('.dropdown-toggle').dropdown();
 })
